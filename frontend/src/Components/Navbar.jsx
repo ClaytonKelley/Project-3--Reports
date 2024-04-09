660a9c2dea52631cd56cfda79f337ab3aa9b7ae7
surucka <conan3383@yahoo.com>
2024-04-09 17:42:19 -0400

    navbar colapse working and create form button

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import profileImage from '../assets/Avatar-Profile.png';
 
-
-
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 
 export default function Navbar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className='fixed left-0 top-0 bottom-0 w-1/3 bg-gray-800 text-white'>
-      <div className="flex flex-col justify-around w-3/4 mx-5 my-5">
-    
-        <div className='relative flex items-center justify-center h-12 w-3/4 mx-auto rounded-full bg-blue-700 hover:bg-blue-300'>
-          <button>Create Report</button>
+    <div className={`${isExpanded ? 'w-3/4' : 'w-1/3'} fixed left-0 top-0 bottom-0 bg-gray-800 text-white transition-width duration-300 overflow-hidden`}>
+      <div className="flex flex-col items-center h-full">
+     
+        <button onClick={toggleNavbar} className="p-2 mt-4 text-white">
+        {isExpanded ?  '<' : '>'}
+      </button>
+
+     
+        <div className="mt-4 mb-2">
+          <img src={profileImage} alt="Profile Avatar" className="rounded-full w-24 h-24" />
         </div>
-        <div className=''>Scroll Bar</div>
-        <ul className='flex flex-col justify-center items-center space-y-2'>
+
+     
+        <div className='relative my-2 flex items-center justify-center h-12 w-3/4 mx-auto rounded-full bg-blue-700 hover:bg-blue-300'>
+          <Link to="/Reportform">
+            <button className="text-white">Create Report</button>
+          </Link>
+        </div>
+
+        <div className='flex-grow overflow-y-auto w-full'>
+          <ul className='flex flex-col items-center space-y-2 py-2'>
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 1</li>
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 2</li>
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 3</li>
@@ -24,15 +46,10 @@ export default function Navbar() {
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 8</li>
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 9</li>
             <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 10</li>
-            <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 11</li>
-            <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 12</li>
-            <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 13</li>
-            <li className='bg-gray-500 rounded-lg w-3/4 h-20 text-center flex justify-center items-center'>REPORT 14</li>
 
-
-        </ul>
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 }
-
--- frontend/src/Components/Reportform.jsx
+++ frontend/src/Components/Reportform.jsx
@@ -1,12 +1,151 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
 
 export default function Reportform() {
-    const [form, Setform] = useState();
+  const [form, setForm] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleCloseForm = () => {
+    window.location.href = '/';
+  }
 
   return (
-    <form>
-        
-    </form>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-800 bg-opacity-50 text-white">
+        <button onClick={handleCloseForm} className="absolute top-0 right-0 mt-2 mr-2 text-red-500 hover:text-red-700 font-bold text-xl">
+        X
+        </button>
+      <form className="bg-white p-6 rounded-lg shadow-md">
+        <div className="mb-4 grid grid-cols-3 gap-4">
+          <div>
+            <label htmlFor="MISSION" className="block mb-2 font-bold text-gray-700">
+              MISSION:
+            </label>
+            <input
+              type="text"
+              id="MISSION"
+              name="MISSION"
+              value={form.MISSION || ''}
+              onChange={handleChange}
+              placeholder="PW1234-24-C1"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="ORGANIZATION" className="block mb-2 font-bold text-gray-700">
+              ORGANIZATION:
+            </label>
+            <input
+              type="text"
+              id="ORGANIZATION"
+              name="ORGANIZATION"
+              value={form.ORGANIZATION || ''}
+              onChange={handleChange}
+              placeholder="53SOPS"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="SYSTEM" className="block mb-2 font-bold text-gray-700">
+              SYSTEM:
+            </label>
+            <input
+              type="text"
+              id="SYSTEM"
+              name="SYSTEM"
+              value={form.SYSTEM || ''}
+              onChange={handleChange}
+              placeholder="WGS 7"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+        </div>
+        <div className="mb-4 grid grid-cols-3 gap-4">
+          <div>
+            <label htmlFor="TEAM" className="block mb-2 font-bold text-gray-700">
+              TEAM:
+            </label>
+            <input
+              type="text"
+              id="TEAM"
+              name="TEAM"
+              value={form.TEAM || ''}
+              onChange={handleChange}
+              placeholder="USS Washington"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="CHOPS" className="block mb-2 font-bold text-gray-700">
+              CHOPS:
+            </label>
+            <input
+              type="text"
+              id="CHOPS"
+              name="CHOPS"
+              value={form.CHOPS || ''}
+              onChange={handleChange}
+              placeholder="CCK"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="IRON" className="block mb-2 font-bold text-gray-700">
+              IRON:
+            </label>
+            <input
+              type="text"
+              id="IRON"
+              name="IRON"
+              value={form.IRON || ''}
+              onChange={handleChange}
+              placeholder="Steel"
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+        </div>
+        <div className="mb-4 grid grid-cols-3 gap-4">
+          <div className="col-span-3 bg-gray-200 rounded-lg p-4">
+            <label htmlFor="bigInput1" className="block mb-2 font-bold text-gray-700">
+              Details:
+            </label>
+            <textarea
+              id="bigInput1"
+              name="bigInput1"
+              value={form.bigInput1 || ''}
+              onChange={handleChange}
+              rows={4} // Set the number of rows
+              placeholder={`
+              Total Power Out (TPO): 
+              Error to bit Noise Ratio: 
+              Location: 
+              Weather:
+              `}
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+              style={{ whiteSpace: 'pre-line', textAlign: 'left' }}
+            />
+          </div>
+          <div className="col-span-3 bg-gray-200 rounded-lg p-4">
+            <label htmlFor="bigInput2" className="block mb-2 font-bold text-gray-700">
+              Comments:
+            </label>
+            <textarea
+              id="bigInput2"
+              name="bigInput2"
+              value={form.bigInput2 || ''}
+              onChange={handleChange}
+              rows={4} // Set the number of rows
+              placeholder="There is shit everywhere, please send someone to clean up this shit."
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
