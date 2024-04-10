5fafa6de92d696fcba293fdc94143ceab32c2340
Clayton <clayton.c.kelley2@gmail.com>
2024-04-11 04:02:31 +0900

    added more api routes and fetchs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Reportform from './Components/Reportform';
 import './App.css';
+import ReportContext from './Components/ReportContext'
 
 
 function App() {
+  const [reportList, setReportList] = React.useState([])
+  React.useEffect(() => {
+    fetch('http://localhost:8080/report_data')
+    .then(raw => raw.json())
+    .then(data => setReportList(data))
+  },[])
   return (
-    <div>
+    <ReportContext.Provider value={{reportList}}>
         <Routes>
           <Route path="/" element={<Navbar/>} />
           <Route path="/Reportform" element={<Reportform/>} />
         </Routes>
-    </div>
+    </ReportContext.Provider>
   );
 }
 
